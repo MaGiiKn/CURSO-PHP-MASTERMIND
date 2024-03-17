8f0f11b6bda3fe7cbf3db7fc6152bdefaff9d9e7
magiikn <brunooviedo212@gmail.com>
2024-03-17 19:30:56 -0300

    User-contacts relation

--- sql/setup.sql
+++ sql/setup.sql
@@ -5,18 +5,17 @@ CREATE DATABASE contacts_app;
 USE contacts_app;
 
 CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY, 
-    name VARCHAR (255),
-    email VARCHAR (255) UNIQUE,
-    password VARCHAR (255)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255) UNIQUE,
+    password VARCHAR(255)
 );
 
 CREATE TABLE contacts (
-    id INT AUTO_INCREMENT PRIMARY KEY, 
-    name VARCHAR (255),
-    phone_number INT (255) 
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    user_id INT NOT NULL,
+    phone_number VARCHAR(255),
+
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
-
-INSERT INTO users (name, email, password) VALUES ('Bruno','hola@gmail.com', '123456789');
-
-INSERT INTO contacts (name, phone_number) VALUES ('Bruno','123456');
