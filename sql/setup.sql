db56638ba7d0833bd30ddbd786d19ec0a4b2005d
magiikn <brunooviedo212@gmail.com>
2024-03-25 20:50:45 -0400

    Add adresses page added

--- sql/setup.sql
+++ sql/setup.sql
@@ -20,10 +20,15 @@ CREATE TABLE contacts (
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
+-- En esta tabla estan las columnas "user_id" que referencia al usuario que creó o añadio esa direccion a este contacto
+-- y "adress_id" referencia al contacto al cual pertenece esa direccion.
+
 CREATE TABLE adress (
     id INT AUTO_INCREMENT PRIMARY KEY,
     adress VARCHAR(255),
     user_id INT NOT NULL,
+    adress_id INT NOT NULL,
 
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (adress_id) REFERENCES contacts(id)
 )
